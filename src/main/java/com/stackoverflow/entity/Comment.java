4a86105df3b89a4946633157dfbb2ccd1cd4f7ec
Jaweria Jawed <jaweriajawed6@gmail.com>
2024-09-16 17:04:34 +0530

    Created tag entity

--- src/main/java/com/stackoverflow/entity/Comment.java
+++ src/main/java/com/stackoverflow/entity/Comment.java
@@ -1,12 +1,16 @@
 package com.stackoverflow.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comments")
 public class Comment {
 
---
+++ src/main/java/com/stackoverflow/entity/Tag.java
@@ -0,0 +1,27 @@
+package com.stackoverflow.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tags")
+public class Tag {
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Question> questions;
+}
