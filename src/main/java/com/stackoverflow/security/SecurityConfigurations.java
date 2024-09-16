5fa229695ec49a64095c144a5811aba0d23a3aa9
bhuvanesh_hj <bhuvanesh.hj.29.2@mountblue.tech>
2024-09-16 15:07:56 +0530

    database config

---
+++ src/main/java/com/stackoverflow/security/SecurityConfigurations.java
@@ -0,0 +1,22 @@
+package com.stackoverflow.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(config -> {
+                    config
+                            .requestMatchers("/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .build();
+    }
+}
