f666101e0a9b30586293d31c6793aad7f564272a
Pradip Borade <pradipborade911@gmail.com>
2024-09-17 11:41:31 +0530

    Added modelMapper Bean configuration.

--- src/main/java/com/stackoverflow/StackoverflowCloneApplication.java
+++ src/main/java/com/stackoverflow/StackoverflowCloneApplication.java
@@ -1,13 +1,23 @@
 package com.stackoverflow;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StackoverflowCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StackoverflowCloneApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper getmodelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
 	}
 
 }
